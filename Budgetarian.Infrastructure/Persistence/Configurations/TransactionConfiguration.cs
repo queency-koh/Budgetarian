d451d2d50b07e2fedf13843cf5923b178cfb7cbb
Queency Koh <Queency.Koh@iridium-technology.com>
2021-09-13 00:48:59 +0800

    Configure Entity

---
+++ Budgetarian.Infrastructure/Persistence/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Budgetarian.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Budgetarian.Infrastructure.Persistence.Configurations
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.Property(t => t.Title)
+                .HasMaxLength(280)
+                .IsRequired();
+
+            builder.Property(t => t.Notes)
+                .HasMaxLength(4000);
+        }
+    }
+}
